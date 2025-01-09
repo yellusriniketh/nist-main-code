6a24c74f6fc941523b088cd78c471ecccf4cf02d
Yellu Sriniketh Reddy <104980641+yellusriniketh@users.noreply.github.com>
2025-01-09 11:02:25 +0000

    commit#

---
+++ Extract-Single-File.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+def extract_data(p):
+    base_url = "https://webbook.nist.gov/cgi/fluid.cgi?Action=Data&Wide=on&ID=C7782447&Type=IsoBar&Digits=5&P={}&THigh=600&TLow=60&TInc=1&RefState=DEF&TUnit=K&PUnit=bar&DUnit=kg%2Fm3&HUnit=kJ%2Fkg&WUnit=m%2Fs&VisUnit=Pa*s&STUnit=N%2Fm"
+    url = base_url.format(p)
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        filename = f"single-webpage_content_{p}.txt"
+        with open(filename, "w", encoding="utf-8") as file:
+            file.write(soup.prettify())
+        print(f"Webpage content saved to '{filename}'.")
+    else:
+        print(f"Failed to retrieve the webpage for P={p}. Status code: {response.status_code}")
+
+# Example usage
+pressure_value = 125  # Replace with the desired pressure value
+extract_data(pressure_value)
