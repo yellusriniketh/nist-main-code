6df9c4ba59ea8aaf016ccca3e96520ceccc19714
Yellu Sriniketh Reddy <104980641+yellusriniketh@users.noreply.github.com>
2025-02-18 09:11:24 +0000

    commit

--- Extract.py
+++ Extract.py
@@ -5,12 +5,12 @@ import os
 
 base_url = "https://webbook.nist.gov/cgi/fluid.cgi?Action=Data&Wide=on&ID=C7782447&Type=IsoBar&Digits=5&P={}&THigh=660&TLow=60&TInc=1&RefState=DEF&TUnit=K&PUnit=bar&DUnit=kg%2Fm3&HUnit=kJ%2Fkg&WUnit=m%2Fs&VisUnit=Pa*s&STUnit=N%2Fm"
 # Create a list to hold the contents of all txt files
-for p in range(1,200):
+for p in range(1,10):
     url = base_url.format(p)
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        filename = f"webpage_content_{p}.txt"
+        filename = f"oxygen_{p}.txt"
         with open(filename, "w", encoding="utf-8") as file:
             file.write(soup.prettify())
         print(f"Webpage content saved to '{filename}'.")
