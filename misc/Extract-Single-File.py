3a4348b6c3a82dad50a2343c45516cf8c68a873d
Yellu Sriniketh Reddy <104980641+yellusriniketh@users.noreply.github.com>
2025-02-18 12:09:23 +0000

    commit

--- misc/Extract-Single-File.py
+++ misc/Extract-Single-File.py
@@ -3,12 +3,12 @@ from bs4 import BeautifulSoup
 import os
 
 def extract_data(p):
-    base_url = "https://webbook.nist.gov/cgi/fluid.cgi?Action=Data&Wide=on&ID=C7782447&Type=IsoBar&Digits=5&P=200&THigh={}&TLow={}&TInc=1&RefState=DEF&TUnit=K&PUnit=bar&DUnit=kg%2Fm3&HUnit=kJ%2Fkg&WUnit=m%2Fs&VisUnit=Pa*s&STUnit=N%2Fm"
+    base_url = "https://webbook.nist.gov/cgi/fluid.cgi?Action=Data&Wide=on&ID=C112403&Type=IsoBar&Digits=5&P={}&THigh=700&TLow=264&TInc=1&RefState=DEF&TUnit=K&PUnit=bar&DUnit=kg%2Fm3&HUnit=kJ%2Fkg&WUnit=m%2Fs&VisUnit=Pa*s&STUnit=N%2Fm"
     url = base_url.format(p)
     response = requests.get(url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
-        filename = f"oxygen_{p}.txt"
+        filename = f"dodecane_{p}.txt"
         with open(filename, "w", encoding="utf-8") as file:
             file.write(soup.prettify())
         print(f"Webpage content saved to '{filename}'.")
@@ -16,5 +16,5 @@ def extract_data(p):
         print(f"Failed to retrieve the webpage for P={p}. Status code: {response.status_code}")
 
 # Example usage
-pressure_value = 200  # Replace with the desired pressure value
+pressure_value = 0.5  # Replace with the desired pressure value
 extract_data(pressure_value)
